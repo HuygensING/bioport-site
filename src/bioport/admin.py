5aca9e3f8fcf2cb5e12ad6d6b4b7550a6c772538
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2009-10-22 20:41:04 +0000

    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@12130 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- src/bioport/admin.py
+++ src/bioport/admin.py
@@ -3,7 +3,7 @@ import grok
 from BioPortRepository.repository import Repository
 import BioPortRepository
 from BioPortRepository.common import  BioPortException
-from BioDes.common import to_ymd, from_ymd
+from NamenIndex.common import to_ymd, from_ymd
 from common import FormatUtilities, RepositoryInterface
 from zope.interface import Interface
 from zope import schema
