82bbe8e576546b93a80767da33fe8dad8d83a688
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2009-10-22 20:17:09 +0000

    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@12126 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

---
+++ src/bioport/person.py
@@ -0,0 +1,8 @@
+import grok
+from BioPortRepository.repository import Repository
+import BioPortRepository
+from BioPortRepository.common import  BioPortException
+from common import FormatUtilities, RepositoryInterface
+from zope.interface import Interface
+from zope import schema
+from app import Personen
