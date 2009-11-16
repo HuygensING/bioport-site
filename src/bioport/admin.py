50eda6d8b8dd20dc0bc98db4314757851dd2e5c4
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2009-11-16 22:51:24 +0000

    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@12524 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- src/bioport/admin.py
+++ src/bioport/admin.py
@@ -96,7 +96,7 @@ class Edit(grok.EditForm):
         self.context.repository().db._update_occupations_table()
         self.redirect(self.url(self))
         
-    @grok.action('Set state of edited persons to "bewerkt" (JG: DELETE THIS BUTTON WHEN DONE)')
+    @grok.action('Set state of edited persons to bewerkt (JG: DELETE THIS BUTTON WHEN DONE)')
     def set_state_to_bewerkt(self, **data):
         from BioPortRepository.upgrade import upgrade_persons
         upgrade_persons(self.context.repository())
