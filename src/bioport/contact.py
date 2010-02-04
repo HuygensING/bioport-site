f4e4d030cec15e4ef2b50c7068043622da08fa59
silvio <silvio@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-02-04 20:17:56 +0000

    Working captcha (without the image yet)
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@13816 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- src/bioport/contact.py
+++ src/bioport/contact.py
@@ -6,17 +6,51 @@ from zope import schema
 from zope.component import getUtility
 from zope.interface import Interface
 from zope.sendmail.interfaces import IMailDelivery
-
+from random import randint
+from zope.app.form.browser.textwidgets import TextWidget
+from bioport.crypt import encrypt
+from bioport.crypt import decrypt
+from grokcore.view.util import url
+from zope.app.form.interfaces import MissingInputError
 grok.context(Bioport)
 
+ENCRYPTION_KEY = 'A verySecretkey!'
+CAPTCHA_LENGTH = 5
+CAPTCHA_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ123456789'
+
+def get_random_sequence():
+    result = ''
+    for i in xrange(CAPTCHA_LENGTH):
+        result += CAPTCHA_ALPHABET[randint(0,len(CAPTCHA_ALPHABET)-1)]
+    return result
 
 class IContact(Interface):
     name = schema.TextLine(title=u"Naam")
     sender = schema.TextLine(title=u"Mailadres")
     text = schema.Text(title=u"Tekst")
+    verification = schema.Text(title=u"verification")
+
+class CaptchaWidget(TextWidget):
+    def getInputValue(self):
+        value = super(CaptchaWidget, self).getInputValue()
+        solution = decrypt(ENCRYPTION_KEY, self.request.form['captcha_text'])
+        if value.upper().replace('0','O') != solution:
+            raise MissingInputError(self, None)
+        return solution
+        
+    def __call__(self):
+        original_widget = super(CaptchaWidget, self).__call__()
+        solution = get_random_sequence()
+        enc_value = encrypt(ENCRYPTION_KEY, solution)
+        base_url = url( self.request,self.context.context)
+        image_url = base_url + '/captcha_image?key=' + enc_value
+        my_widget = original_widget + ' <img src="%s">' % image_url
+        my_widget = original_widget + ' <input type="hidden" name="captcha_text" value="%s">' % enc_value        
+        return my_widget
 
 class ContactForm(grok.AddForm, RepositoryView):
     form_fields = grok.AutoFields(IContact)
+    form_fields["verification"].custom_widget = CaptchaWidget
     @grok.action('Submit')
     def submit(self, **kwargs):
         "Process input and send email"
@@ -24,6 +58,7 @@ class ContactForm(grok.AddForm, RepositoryView):
         email_from = self.context['admin'].EMAIL_FROM_ADDRESS
         send_email(email_from, kwargs['sender'], subject, kwargs['text']) 
         self.redirect(self.application_url() + '/contactok')
+
 
 class Contact(grok.View, RepositoryView):
     pass
--- src/bioport/contact_tests.py
+++ src/bioport/contact_tests.py
@@ -5,14 +5,25 @@ Do a functional test on the app.
 """
 from bioport.tests import FunctionalTestCase
 from bioport.tests import messages
+from bioport.contact import ENCRYPTION_KEY
+from bioport.crypt import decrypt
+import re
 
 class ContactFormTest(FunctionalTestCase):
     def test_contact_form(self):
+        messages_before = len(messages)
         self.app['admin'].EMAIL_FROM_ADDRESS = 'admin@example.com'
         self.browser.open(self.base_url + '/contact')
         self.browser.getControl('Naam').value = 'Me'
         self.browser.getControl('Mailadres').value = 'me@example.com'
         self.browser.getControl('Tekst').value = 'Hey'
+        self.browser.getControl('verification').value = 'wrongvalue'
+        self.browser.getControl('Submit').click()
+        self.failUnlessEqual(len(messages), messages_before)
+        # Solve the captcha!
+        captcha_crypt = re.findall(r'captcha_text[^>]+value="([^"]+)', self.browser.contents)[0]
+        captcha_solution = decrypt(ENCRYPTION_KEY,captcha_crypt)
+        self.browser.getControl('verification').value = captcha_solution
         self.browser.getControl('Submit').click()
         self.failUnless('me@example.com' in messages[-1]['dest'])
         self.failUnlessEqual(messages[-1]['source'], 'admin@example.com')
