cbe8abe62c8e4a6ee06d4c7b6dba39af6422a898
silvio <silvio@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-02-04 16:04:33 +0000

    Working email sending (with no captcha)
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@13810 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- src/bioport/contact.py
+++ src/bioport/contact.py
@@ -1,23 +1,41 @@
+import email.Header
 import grok
-from app import RepositoryView
 from app import Bioport
-from zope.interface import Interface
+from app import RepositoryView
 from zope import schema
+from zope.component import getUtility
+from zope.interface import Interface
+from zope.sendmail.interfaces import IMailDelivery
 
 grok.context(Bioport)
 
 
 class IContact(Interface):
-    naam = schema.TextLine(title=u"Naam")
-    mailadres = schema.TextLine(title=u"Mailadres")
-    tekst = schema.Text(title=u"Tekst")
+    name = schema.TextLine(title=u"Naam")
+    sender = schema.TextLine(title=u"Mailadres")
+    text = schema.Text(title=u"Tekst")
 
 class ContactForm(grok.AddForm, RepositoryView):
     form_fields = grok.AutoFields(IContact)
     @grok.action('Submit')
     def submit(self, **kwargs):
         "Process input and send email"
+        subject = kwargs['name'] + ' has submitted some feedback'
+        send_email('silviot@gmail.com', kwargs['sender'], subject, kwargs['text']) 
+        self.redirect(self.application_url() + '/contactok')
 
 class Contact(grok.View, RepositoryView):
     pass
+
+class ContactOk(grok.View, RepositoryView):
+    pass
+
+
+def send_email(sender, recipient, subject, body):
+    msg = email.MIMEText.MIMEText(body.encode('UTF-8'), 'plain', 'UTF-8')
+    msg["From"] = sender
+    msg["To"] = recipient
+    msg["Subject"] = email.Header.Header(subject, 'UTF-8')
+    mailer = getUtility(IMailDelivery, 'bioport.mailer')
+    mailer.send(sender, [recipient], msg.as_string())
 
