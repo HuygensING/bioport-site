485710e2b3860ed720882cd0b0ef726c3de728d9
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-02-15 16:42:25 +0000

    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@14172 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- src/bioport/biodes.py
+++ src/bioport/biodes.py
@@ -8,6 +8,7 @@ class BioDes(grok.Container):
     grok.template('index')
     def repository(self, user):
         return self.__parent__.repository(user)
+    
 class Beschrijving(grok.View, RepositoryView):
     pass
 
