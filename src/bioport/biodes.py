ea0b5b37df38dd7f8335822d96098535e39cfcf0
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-02-15 15:06:17 +0000

    added biodes page
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@14159 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

---
+++ src/bioport/biodes.py
@@ -0,0 +1,24 @@
+import grok
+from bioport.app import Bioport
+from bioport.app import RepositoryView
+
+import zope.interface
+
+class BioDes(grok.Container):
+    grok.template('index')
+    def repository(self, user):
+        return self.__parent__.repository(user)
+class Beschrijving(grok.View, RepositoryView):
+    pass
+
+class Index(grok.View, RepositoryView):
+    pass
+
+class Persoonsnaam(grok.View, RepositoryView):
+    pass
+
+class Schemas(grok.View, RepositoryView):
+    pass
+
+class Voorbeelden(grok.View, RepositoryView):
+    pass
