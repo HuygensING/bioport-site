10d351a3a4d1f93ded1900afeb04d0271a8a1de1
silvio <silvio@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-03-23 14:35:15 +0000

    search form translated
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@14961 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

---
+++ src/bioport/dummy_translations.py
@@ -0,0 +1,23 @@
+"""
+The purpose of this file is to provide some strings discoverable 
+by i18n tools.
+"""
+from bioport import BioportMessageFactory as _
+
+def categories_for_biographies():
+    _("Adel en vorstenhuizen")
+    _("Bedrijfsleven")
+    _("Beeldende kunsten &amp; vormgeving")
+    _("Kerk en godsdienst")
+    _("Koloniale en overzeese betrekkingen en handel")
+    _("Krijgsmacht en oorlog")
+    _("Letterkunde")
+    _("Maatschappelijke bewegingen")
+    _("Rechtspraak")
+    _("Onderwijs &amp; wetenschappen")
+    _("Politiek en bestuur")
+    _("Radio en TV")
+    _("Sport en vrije tijd")
+    _("Uitvoerende kunsten")
+    _("Zorg")
+    _("Overig")
