bc3de30a848c64458f9af14abe4c2f51e0e4dec1
silvio <silvio@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-05-21 15:55:04 +0000

    minor progress in fuzzy date parsing
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@16013 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- src/bioport/fuzzy_search.py
+++ src/bioport/fuzzy_search.py
@@ -2,8 +2,8 @@
 This module converts human-written sentences expressing dates or date ranges
 into distionaries that represent the query, that can be further
 elaburated and fed to BioPortRepository.
-Output fields:
-    year_min
+Output fields (all integer types):
+    year_min 
     year_max
     month
     day
@@ -25,6 +25,13 @@ def get_search_query(original_search_text):
         # we have a raw number, let's treat it as a year
         result['year_max'] = int(search_text)
         result['year_min'] = int(search_text)
+    elif search_text.count('/') == 2:
+        # two slashes: we have a full date
+        day, month, year = search_text.split('/')
+        result['day'] = int(day)
+        result['month'] = int(month)
+        result['year_min'] = int(year)
+        result['year_max'] = int(year)
     return result
 
 
@@ -32,7 +39,15 @@ class FuzzySearchTest(unittest.TestCase):
     def run_test(self, query_text, expected_query):
         effective_query = get_search_query(query_text)
         self.assertEqual(expected_query, effective_query)
-    def test_fuzzy_single_year(self):
+    def test_single_year(self):
         expected_result = {'year_min': 1920, 'year_max': 1920}
         self.run_test('  1920 ', expected_result)
+    def test_complete_date(self):
+        expected_result = {
+            'year_min': 1978,
+            'year_max': 1978,
+            'month': 10,
+            'day' : 12,
+        }
+        self.run_test('12/10/1978', expected_result)
 
