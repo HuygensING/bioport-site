322b70aaf692ee31cbe96ae36c4e3cca7aacd495
giampaolo <giampaolo@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-07-15 11:14:52 +0000

    fix around pstats module which might be missing on some linux
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@17071 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- src/bioport/utils.py
+++ src/bioport/utils.py
@@ -1,56 +1,60 @@
 #!/usr/bin/env python
 
 import hotshot
-import hotshot.stats
 import tempfile
+try:
+    import pstats
+except ImportError:
+    pstats = None  # might be missing on some Linuxes
 
 
-def profile(sort=('time', 'calls'), lines=30, strip_dirs=False):
-    """A decorator which profiles a callable.
+if pstats is not None:
+    def profile(sort=('time', 'calls'), lines=30, strip_dirs=False):
+        """A decorator which profiles a callable.
 
-    Example usage:
+        Example usage:
 
-    >>> class Foo:
-    ...
-    ...     @profile()
-    ...     def bar(self):
-    ...          return "ciao"
-    ... 
-    >>> Foo().bar()
-             1 function calls in 0.000 CPU seconds
+        >>> class Foo:
+        ...
+        ...     @profile()
+        ...     def bar(self):
+        ...          return "ciao"
+        ... 
+        >>> Foo().bar()
+                 1 function calls in 0.000 CPU seconds
 
-       Ordered by: cumulative time
+           Ordered by: cumulative time
 
-       ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-            1    0.000    0.000    0.000    0.000 <stdin>:2(bar)
-            0    0.000             0.000          profile:0(profiler)
+           ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+                1    0.000    0.000    0.000    0.000 <stdin>:2(bar)
+                0    0.000             0.000          profile:0(profiler)
 
 
-    'ciao'
-    >>> 
-    """
-    def _outer(f):
-        def _inner(*args, **kwargs):
-            #
-            file = tempfile.NamedTemporaryFile()
-            prof = hotshot.Profile(file.name)
-            try:
-                ret = prof.runcall(f, *args, **kwargs)
-            finally:
-                prof.close()
-            
-            stats = hotshot.stats.load(file.name)
-            if strip_dirs:
-                stats.strip_dirs()
-            if isinstance(sort, tuple):
-                stats.sort_stats(*sort)
-            else:
-                stats.sort_stats(sort)
-            stats.print_stats(lines)
+        'ciao'
+        >>> 
+        """
+        def _outer(f):
+            def _inner(*args, **kwargs):
+                #
+                file = tempfile.NamedTemporaryFile()
+                prof = hotshot.Profile(file.name)
+                try:
+                    ret = prof.runcall(f, *args, **kwargs)
+                finally:
+                    prof.close()
 
-            return ret
-        return _inner
+                stats = pstats.Stats(file.name)
+                if strip_dirs:
+                    stats.strip_dirs()
+                if isinstance(sort, tuple):
+                    stats.sort_stats(*sort)
+                else:
+                    stats.sort_stats(sort)
+                stats.print_stats(lines)
 
-    return _outer
+                return ret
+            return _inner
+
+        return _outer
 
 
