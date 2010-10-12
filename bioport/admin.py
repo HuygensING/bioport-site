16923db3d006e2272c29c8167c798eee6cc1cf5b
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-10-12 10:47:40 +0000

    fixed bug in refreshing of view
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@18815 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- bioport/admin.py
+++ bioport/admin.py
@@ -693,6 +693,7 @@ class Persoon(app.Persoon, grok.EditForm, RepositoryView):
 #        self.repository().save_person(self.person)
         self.repository().save_biography(self.bioport_biography)
         #we need to reload merged_biography because changes are not automatically picked up
+        self.person = self.repository().get_person(self.bioport_id)
         self.merged_biography  = self.person.get_merged_biography()
 
     @grok.action('bewaar geslacht', name="save_sex")
