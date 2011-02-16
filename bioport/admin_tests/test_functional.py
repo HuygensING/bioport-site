f0d0653cd07b25e08cf5ea7be726fdb289e0563c
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2011-02-16 13:17:11 +0000

    fix ambiguity in tests;fix search for status in most similar
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@21092 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- bioport/admin_tests/test_functional.py
+++ bioport/admin_tests/test_functional.py
@@ -97,7 +97,7 @@ class SimpleSampleFunctionalTest(FunctionalTestCase):
         browser = Browser('http://localhost/app/admin/persoon?bioport_id=%s' % bioport_id1)
         browser.getControl(name='personname', index=0).value = 'changed name0'
         browser.getControl(name='name_new').value='new name1'
-        browser.getForm().getControl(name='form.actions.save_everything').click()
+        browser.getForm().getControl(name='form.actions.save_everything', index=0).click()
         
         self.assertEqual(browser.getControl(name='personname', index=0).value , 'changed name0')
         self.assertEqual(browser.getControl(name='personname', index=1).value , 'new name1')
@@ -198,7 +198,7 @@ class SimpleSampleFunctionalTest(FunctionalTestCase):
         form.getControl(name='birth_y').value=''
         form.getControl(name='birth_text').value=''
         form.getControl(name='personname'). value = 'xxx'
-        form.getControl(name='form.actions.save_everything').click()
+        form.getControl(name='form.actions.save_everything', index=0).click()
        
         #go the the public view 
         browser.getLink('in portaal').click()
@@ -208,7 +208,7 @@ class SimpleSampleFunctionalTest(FunctionalTestCase):
         browser.open(edit_url)
         form = browser.getForm(index=0)
         form.getControl(name='birth_y').value='1111'
-        form.getControl(name='form.actions.save_everything').click()
+        form.getControl(name='form.actions.save_everything', index=0).click()
         browser.open(public_url)
         assert re.findall('1111', browser.contents, re.DOTALL)
         
@@ -218,7 +218,7 @@ class SimpleSampleFunctionalTest(FunctionalTestCase):
         form.getControl(name='death_y').value='2222'
         form.getControl(name='death_m').value=['2']
         form.getControl(name='death_d').value='1'
-        form.getControl(name='form.actions.save_everything').click()
+        form.getControl(name='form.actions.save_everything', index=0).click()
         form = browser.getForm(index=0)
         self.assertEqual(form.getControl(name='death_y').value,'2222')
         self.assertEqual(form.getControl(name='death_m').value,['2'])
@@ -229,14 +229,14 @@ class SimpleSampleFunctionalTest(FunctionalTestCase):
         browser.open(edit_url)
         form = browser.getForm(index=0)
         form.getControl(name='funeral_y').value='3333'
-        form.getControl(name='form.actions.save_everything').click()
+        form.getControl(name='form.actions.save_everything', index=0).click()
         browser.open(public_url)
         assert re.findall('3333', browser.contents, re.DOTALL)
         
         browser.open(edit_url)
         form = browser.getForm(index=0)
         form.getControl(name='baptism_y').value='4444'
-        form.getControl(name='form.actions.save_everything').click()
+        form.getControl(name='form.actions.save_everything', index=0).click()
         browser.open(public_url)
         assert re.findall('4444', browser.contents, re.DOTALL)
         
@@ -244,7 +244,7 @@ class SimpleSampleFunctionalTest(FunctionalTestCase):
         browser.open(edit_url)
         form = browser.getForm(index=0)
         form.getControl(name='sex').value=['2']
-        form.getControl(name='form.actions.save_everything').click()
+        form.getControl(name='form.actions.save_everything', index=0).click()
         browser.open(public_url)
         assert re.findall('vrouw', browser.contents, re.DOTALL)
         
@@ -253,7 +253,7 @@ class SimpleSampleFunctionalTest(FunctionalTestCase):
         form = browser.getForm(index=0)
         s = 'Given the existence as uttered forth in the public works of Puncher and Wattmann of a personal God quaquaquaqua'
         form.getControl(name='remarks_bioport_editor').value=s
-        form.getControl(name='form.actions.save_everything').click()
+        form.getControl(name='form.actions.save_everything', index=0).click()
         browser.open(edit_url)
         form = browser.getForm(index=0)
         self.assertEqual(form.getControl(name='remarks_bioport_editor').value, s)
@@ -265,7 +265,7 @@ class SimpleSampleFunctionalTest(FunctionalTestCase):
         browser.open(edit_url)
         form = browser.getForm(index=0)
         form.getControl(name='remarks').value=s
-        form.getControl(name='form.actions.save_everything').click()
+        form.getControl(name='form.actions.save_everything', index=0).click()
         browser.open(edit_url)
         form = browser.getForm(index=0)
         self.assertEqual(form.getControl(name='remarks').value, s)
@@ -297,7 +297,7 @@ class SimpleSampleFunctionalTest(FunctionalTestCase):
             except:
                 print '********', k, v
                 raise
-        form.getControl(name='form.actions.save_everything').click()
+        form.getControl(name='form.actions.save_everything', index=0).click()
         form = browser.getForm(index=0)
         for k,v in vals:
             self.assertEqual(form.getControl(name=k).value, v)
@@ -340,7 +340,7 @@ class NewFieldsTestCase(FunctionalTestCase):
         browser.open(edit_url)
         browser.getControl(name='reference_new_url').value = "http://url1"
         browser.getControl(name='reference_new_text').value = 'url1'
-        browser.getControl(name='form.actions.save_everything').click()
+        browser.getControl(name='form.actions.save_everything', index=0).click()
         
         #now we should have a new element in the form with our new relation info
         assert len(get_identifiers()) == 1
@@ -368,7 +368,7 @@ class NewFieldsTestCase(FunctionalTestCase):
         identifier = get_identifiers()[2]
         browser.getControl(name='reference_%s_url' % identifier).value = "http://url4"
         browser.getControl(name='reference_%s_text' % identifier).value = 'url4'
-        browser.getControl(name='form.actions.save_everything').click()       
+        browser.getControl(name='form.actions.save_everything', index=0).click()       
         
         #see if changes stick
         self.assertEqual(len(get_identifiers()), 3)
@@ -391,7 +391,7 @@ class NewFieldsTestCase(FunctionalTestCase):
         identifier = get_identifiers()[0]
         browser.getControl(name='reference_%s_url' % identifier).value = "http://url5"
         browser.getControl(name='reference_%s_text' % identifier).value = 'url5'
-        browser.getControl(name='form.actions.save_everything').click()       
+        browser.getControl(name='form.actions.save_everything', index=0).click()       
         
         self.assertEqual(browser.getControl(name='reference_%s_url' % identifier).value, 'http://url5')
         self.assertEqual(browser.getControl(name='reference_%s_text' % identifier).value , 'url5')
@@ -400,30 +400,30 @@ class NewFieldsTestCase(FunctionalTestCase):
         browser = self._open_edit_url()
         #add a state
         browser.getControl(name='religion_id').value = ['1']
-        browser.getControl(name='form.actions.save_everything').click()
+        browser.getControl(name='form.actions.save_everything', index=0).click()
         self.assertEqual(browser.getControl(name='religion_id').value , ['1'])
         browser.getControl(name='religion_id').value = ['']
-        browser.getControl(name='form.actions.save_everything').click()
+        browser.getControl(name='form.actions.save_everything', index=0).click()
         self.assertEqual(browser.getControl(name='religion_id').value , [''])
    
         
-'''
     def test_edit_categories(self):    
         browser = self._open_edit_url()
         edit_url = browser.url
         #add a state
         browser.getControl(name='category_id', index=0).value = ['1']
         browser.getControl(name='category_id', index=1).value = ['2']
-        browser.getControl(name='form.actions.save_everything').click()
+        browser.getControl(name='form.actions.save_everything', index=0).click()
         
         self.assertEqual(browser.getControl(name='category_id', index=0).value , ['1'])
         self.assertEqual(browser.getControl(name='category_id', index=1).value , ['2'])
         
         browser.getControl(name='category_id', index=0).value = ['']
-        browser.getControl(name='form.actions.save_everything').click()
+        browser.getControl(name='form.actions.save_everything', index=0).click()
         self.assertEqual(browser.getControl(name='category_id', index=0).value , ['2'])
         self.assertEqual(browser.getControl(name='category_id', index=1).value , [''])
         
+'''
     def test_edit_states(self):
         browser = self._open_edit_url()
         edit_url = browser.url
