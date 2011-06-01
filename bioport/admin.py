dd935d4cd1bc3034a76243f79619ea2fe645598e
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2011-06-01 11:13:57 +0000

    fixed bug in similarity
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@22591 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- bioport/admin.py
+++ bioport/admin.py
@@ -127,7 +127,7 @@ class Edit(grok.EditForm,RepositoryView):
     def refresh_similarity_cache(self, **data): 
         refresh = bool(self.request.get('refresh'))
         sid = self.request.get('source_id')
-        self.repository().db.fill_similarity_cache(refresh=refresh, source_id=sid, start=request.get('start'))
+        self.repository().db.fill_similarity_cache(refresh=refresh, source_id=sid, start=self.request.get('start'))
         self._redirect_with_msg('finished')
         
     @grok.action('Assign category to source', name='assign_category_to_source')
