e83986ffb115b602be2ec8a418c61a5a5b66113c
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2012-03-08 08:57:47 +0000

    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/site/trunk@25543 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- setup.py
+++ setup.py
@@ -1,29 +1,38 @@
-
-      install_requires=['setuptools',
-                        'grok',
-                        'grokui.admin',
-                        'z3c.testsetup',
-                        'grokcore.startup',
-                        'plone.memoize',
+from setuptools import setup, find_packages
+    
+version = '2.0.0'
+    
+setup(name='bioport',
+      version=version,
+      packages=['bioport'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'grok',
+        'grokui.admin',
+        'z3c.testsetup',
+        'grokcore.startup',
+        'plone.memoize',
 #                        'zope.app.cache==3.4.0',
-                        'zope.app.cache',
-                        'zope.app.testing',
-                        'zope.testbrowser',
-                        'megrok.chameleon',
-                        'z3c.batching',
-                        'MySQL-python',
-                        'sqlalchemy',
-                        'python-Levenshtein',
+        'zope.app.cache',
+        'zope.app.testing',
+        'zope.testbrowser',
+        'megrok.chameleon',
+        'z3c.batching',
+        'MySQL-python',
+        'sqlalchemy',
+        'python-Levenshtein',
 #                        'zope.sendmail==3.5',
-                        'zope.sendmail',
-                        'captchaimage', # This probably requires libfreetype6-dev
-                        'FormEncode',
-                        'collective.monkeypatcher',
-                        'bioport_repository',
-                        'names',
-                        'gerbrandyutils',
-                        'mobile.sniffer',
-                        ],
+        'zope.sendmail',
+        'captchaimage', # This probably requires libfreetype6-dev
+        'FormEncode',
+        'collective.monkeypatcher',
+        'bioport_repository',
+        'names',
+        'gerbrandyutils',
+        'mobile.sniffer',
+        ],
       
       
       
